419bfa8ea90bccb51bf70e5fba44cdd17d33bf5b
tung phan <tung9898989898.com>
2022-01-13 23:30:13 +0700

    first commit

--- src/data.js
+++ src/data.js
@@ -1,24 +1,24 @@
 /* eslint-disable linebreak-style */
 export const data = [
     {
-        id: 1, title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh “Ong vàng Poly”, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt", img: "../src/components/img/anh1.jpg",
+        id: 1, title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh “Ong vàng Poly”, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt", img: "https://kenhphimtv.com/wp-content/uploads/2020/07/29736-au-trung-tinh-nghich-phan-2.jpg ",
     },
     {
-        id: 2, title: "Sinh viên khối ngành Kinh tế học cách “chạm” vào cảm xúc", desc: "Với sự tham gia của chuyên gia trong lĩnh vực kinh doanh, chương trình đã thu hút đông đảo sinh viên Cao đẳng FPT Polytechnic tham dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi", img: "../src/components/img/anh2.jpg",
+        id: 2, title: "Sinh viên khối ngành Kinh tế học cách “chạm” vào cảm xúc", desc: "Với sự tham gia của chuyên gia trong lĩnh vực kinh doanh, chương trình đã thu hút đông đảo sinh viên Cao đẳng FPT Polytechnic tham dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi", img: "https://kenhphimtv.com/wp-content/uploads/2020/07/29736-au-trung-tinh-nghich-phan-2.jpg ",
     },
     {
-        id: 3, title: "Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury", desc: "Với triết lý đào tạo “Thực học – Thực nghiêp”, Cao đẳng FPT Polytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vị doanh nghiệp", img: "../src/components/img/anh3.jpg",
+        id: 3, title: "Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury", desc: "Với triết lý đào tạo “Thực học – Thực nghiêp”, Cao đẳng FPT Polytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vị doanh nghiệp", img: "https://kenhphimtv.com/wp-content/uploads/2020/07/29736-au-trung-tinh-nghich-phan-2.jpg ",
     },
 
 ];
 export const activate = [
     {
-        id: 1, title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh “Ong vàng Poly”, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt", img: "../src/components/img/anh1.jpg",
+        id: 1, title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh “Ong vàng Poly”, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt", img: "https://kenhphimtv.com/wp-content/uploads/2020/07/29736-au-trung-tinh-nghich-phan-2.jpg ",
     },
     {
-        id: 2, title: "Sinh viên khối ngành Kinh tế học cách “chạm” vào cảm xúc", desc: "Với sự tham gia của chuyên gia trong lĩnh vực kinh doanh, chương trình đã thu hút đông đảo sinh viên Cao đẳng FPT Polytechnic tham dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi", img: "../src/components/img/anh2.jpg",
+        id: 2, title: "Sinh viên khối ngành Kinh tế học cách “chạm” vào cảm xúc", desc: "Với sự tham gia của chuyên gia trong lĩnh vực kinh doanh, chương trình đã thu hút đông đảo sinh viên Cao đẳng FPT Polytechnic tham dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi", img: "https://kenhphimtv.com/wp-content/uploads/2020/07/29736-au-trung-tinh-nghich-phan-2.jpg ",
     },
     {
-        id: 3, title: "Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury", desc: "Với triết lý đào tạo “Thực học – Thực nghiêp”, Cao đẳng FPT Polytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vị doanh nghiệp", img: "../src/components/img/anh3.jpg",
+        id: 3, title: "Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury", desc: "Với triết lý đào tạo “Thực học – Thực nghiêp”, Cao đẳng FPT Polytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vị doanh nghiệp", img: "https://kenhphimtv.com/wp-content/uploads/2020/07/29736-au-trung-tinh-nghich-phan-2.jpg ",
     },
 ];
