3362e692cee559d8bf1f737ca6c5c731727fe46f
tung phan <tung9898989898.com>
2022-01-14 18:16:12 +0700

    av

--- src/components/banner.js
+++ src/components/banner.js
@@ -2,7 +2,7 @@
 const Banner = {
     runder() {
         return `
-        <img  src="https://whoisdhillon.files.wordpress.com/2015/07/jw2.jpg" alt="" srcset="" width="100%" >
+        <img  src="https://i.ytimg.com/vi/-QXOFCgm0Lo/maxresdefault.jpg" alt="" srcset="" width="100%" >
         `;
     },
 };
